e3fb1215e80a76a5eddeb30357526d559bbe8449
Julia <juliaduch33@gmail.com>
2022-10-23 17:33:54 +0200

    dostosowanie do pep8

--- zadanie_2.py
+++ zadanie_2.py
@@ -1,20 +1,25 @@
-#2. a Utwórz funkcję, która otrzyma w parametrze listę 5 imion, a następnie wyświetli każde z nich.
+# 2. a Utwórz funkcję, która otrzyma w parametrze listę 5 imion,
+# a następnie wyświetli każde z nich.
 
 def print_names(names):
     for name in names:
         print(name)
 
-print_names(['Julia','Magda','Ola','Asia','Paulina'])
 
-# 2. b. Utwórz funkcję, która otrzyma w parametrze listę zawierającą 5 dowolnych liczb, każdy jej element pomnoży
+print_names(['Julia', 'Magda', 'Ola', 'Asia', 'Paulina'])
+
+# 2. b. Utwórz funkcję, która otrzyma w parametrze listę zawierającą
+# 5 dowolnych liczb, każdy jej element pomnoży
 # przez 2, a na końcu ją zwróci. Zadanie należy wykonać w 2 wersjach:
 # i. Wykorzystując pętle for .
 # ii. Wykorzystując listę składaną.
+
 
 def multiply(numbers):
     for i in range(len(numbers)):
         numbers[i] = numbers[i]*2
     return numbers
+
 
 def multiplyD(numbers):
     numbers = [number*2 for number in numbers]
@@ -24,7 +29,8 @@ def multiplyD(numbers):
 print(multiply([1, 2, 3, 4, 5]))
 print(multiplyD([1, 2, 3, 4, 5]))
 
-# c. Utwórz funkcję, która otrzyma w parametrze listę 10 liczb (rekomendowane wykorzystanie funkcji range ),
+# c. Utwórz funkcję, która otrzyma w parametrze listę
+# 10 liczb (rekomendowane wykorzystanie funkcji range ),
 # a następnie wyświetli jedynie parzyste elementy.
 
 
@@ -37,7 +43,8 @@ def parzyste(numbers):
 parzyste([10, 12, 14, 13, 11, 8, 7, 9, 0, 2])
 
 
-# d. Utwórz funkcję, która otrzyma w parametrze listę 10 liczb (rekomendowane wykorzystanie funkcji range ),
+# d. Utwórz funkcję, która otrzyma w parametrze listę
+# 10 liczb (rekomendowane wykorzystanie funkcji range ),
 # a następnie wyświetli co drugi element.
 
 def every_other_el(numbers):
