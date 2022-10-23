55d6038587e4d1aa3c2f53ed8f15a0b6f7f1b320
Julia <juliaduch33@gmail.com>
2022-10-23 17:19:02 +0200

    modyfikacja do standardu pep8

--- zadanie_1.py
+++ zadanie_1.py
@@ -1,23 +1,20 @@
-# Stworzyć klasę Student , która posiada 2 parametry (name i marks) oraz jedną metodę is_passed, która zwraca wartość
-# logiczną, pozytywną gdy średnia ocen jest > 50 w przeciwnym przypadku negatywną. Następnie należy stworzyć 2 przykładowe
-# obiekty klasy, tak aby dla pierwszego obiektu metoda zwracała true , a dla drugiego false .
-
 class Student:
-    def __init__(self,name,marks):
-        self.name=name
-        self.marks=marks
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
 
     def is_passed(self):
-        if sum(self.marks)/len(self.marks)>50:
+        if sum(self.marks)/len(self.marks) > 50:
             return True
         else:
             return False
+
     def __str__(self):
         return f'Student  {self.name}, with marks {self.marks}'
 
 
-student1 = Student('Julia',[100,200,300])
-student2 = Student('Jakub', [3,4,6])
+student1 = Student('Julia', [100, 200, 300])
+student2 = Student('Jakub', [3, 4, 6])
 
 print(student1.is_passed())
 print(student2.is_passed())
--- zadanie_2.py
+++ zadanie_2.py
@@ -1,33 +1,20 @@
-# 2. Stworzyć następujące klasy:
-# Library (klasa opisująca bibliotekę), posiadająca pola: city, street, zip_code, open_hours (str), phone
-# Employee (klasa opisująca pracownika biblioteki), posiadająca pola: first_name,last_name,hire_date
-# birth_date,city,street,zip_code,phone
-# Book (klasa opisująca książkę), posiadająca pola,library,publication_date,author_name,author_surname,number_of_pages
-# Order (klasa opisująca zamówienie), posiadająca pola:employee,student,books (lista obiektów klasy Book),order_date
-# Dodatkowo:
-# Każda klasa ma mieć zaimplementowaną metodę __str__ , która będzie opisywała obiekt oraz ewentualne obiekty
-# znajdujące się w tym obiekcie (np. obiekt Library w obiekcie Book).
-# Pola w klasie mają być zdefiniowane jako atrybuty ustawiane podczas tworzenia instancji klasy za pośrednictwem
-# konstruktora.
-# Stworzyć 2 biblioteki (2 instancje klasy), 5 książek, 3 pracowników, 3 studentów, oraz 2 zamówienia.
-# Wyświetlić oba zamówienia ( print )
 class Student:
-    def __init__(self,name,marks):
-        self.name=name
-        self.marks=marks
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
 
     def is_passed(self):
-        if sum(self.marks)/len(self.marks)>50:
+        if sum(self.marks)/len(self.marks) > 50:
             return True
         else:
             return False
+
     def __str__(self):
         return f'Student  {self.name}, with marks {self.marks}'
 
 
-
 class Library:
-    def __init__(self,city,street,zip_code,open_hours,phone):
+    def __init__(self, city, street, zip_code, open_hours, phone):
         self.city = city
         self.street = street
         self. zip_code = zip_code
@@ -35,11 +22,13 @@ class Library:
         self.phone = phone
 
     def __str__(self):
-        return f'Library in  {self.city},{self.street} {self.zip_code}, open on {self.open_hours}, phone number {self.phone}'
+        return f'Library in  {self.city},{self.street} {self.zip_code}, ' \
+               f'open on {self.open_hours}, phone number {self.phone}'
 
 
 class Employee:
-    def __init__(self,first_name, last_name, hire_date,birth_date, city, street, zip_code,phone):
+    def __init__(self, first_name, last_name, hire_date, birth_date,
+                 city, street, zip_code, phone):
         self.first_name = first_name
         self.last_name = last_name
         self.hire_date = hire_date
@@ -50,12 +39,16 @@ class Employee:
         self.phone = phone
 
     def __str__(self):
-        return f'Employee {self.first_name} {self.last_name}, date of birth {self.birth_date} hired on {self.hire_date},'\
-        f'lives in {self.city} {self.street} {self.zip_code}, phone number {self.phone}'
+        return f'Employee {self.first_name} {self.last_name}, ' \
+               f'date of birth {self.birth_date} ' \
+               f'hired on {self.hire_date}, ' \
+               f'lives in {self.city} {self.street} {self.zip_code}, ' \
+               f'phone number {self.phone}'
 
 
 class Book:
-    def __init__(self,library: Library,publication_date,author_name,author_surname,number_of_pages):
+    def __init__(self, library: Library, publication_date, author_name,
+                 author_surname, number_of_pages):
         self.library = library
         self.publication_date = publication_date
         self.author_name = author_name
@@ -63,12 +56,16 @@ class Book:
         self.number_of_pages = number_of_pages
 
     def __str__(self):
-        return f'Book from {self.library}, by {self.author_name} {self.author_surname},publication date {self.publication_date}, number of pages {self.number_of_pages}'
+        return f'Book from {self.library}, ' \
+               f'by {self.author_name} {self.author_surname},' \
+               f'publication date {self.publication_date}, ' \
+               f'number of pages {self.number_of_pages}'
 
 
 class Order:
 
-    def __init__(self,employee:Employee,student:Student,books:list,order_date):
+    def __init__(self, employee: Employee, student: Student,
+                 books: list, order_date):
         self.employee = employee
         self.student = student
         self.books = books
@@ -81,27 +78,33 @@ class Order:
         return all
 
     def __str__(self):
-        return f'Order by {self.employee}, \nfor {self.student}, \ncontains: {self.books_description()} , \nmade on {self.order_date}'
+        return f'Order by {self.employee}, \nfor {self.student}, ' \
+               f'\ncontains: {self.books_description()} ,' \
+               f' \nmade on {self.order_date}'
 
-library1 = Library('Warszawa','Kwiatowa','10-200','8-20','123456789')
-library2 = Library('Krakow','Kotlarska','40-200','7-16','188888889')
 
-book1 = Book(library1,'20.09.2010','James','Mcgill','321')
-book2 = Book(library1,'11.11.2010','Arthur','Fresh','366')
-book3 = Book(library2,'02.02.2010','Agatha','Esd','491')
-book4 = Book(library2,'15.09.2020','Jules','Bin','78')
-book5 = Book(library1,'20.09.2002','Coss','Sin','100')
+library1 = Library('Warszawa', 'Kwiatowa', '10-200', '8-20', '123456789')
+library2 = Library('Krakow', 'Kotlarska', '40-200', '7-16', '188888889')
 
-employee1 = Employee('Ala','Kowalska','20.01.2021','01.01.1999','Bytom','Kwaka','42-600','098765432')
-employee2 = Employee('Robert','Kowal','20.11.2021','12.12.1999','Chorzow','Lipowa','42-600','088323432')
-employee3 = Employee('Macej','Polska','21.01.2021','09.09.1999','T.Gory','Lipna','42-600','1424162')
+book1 = Book(library1, '20.09.2010', 'James', 'Mcgill', '321')
+book2 = Book(library1, '11.11.2010', 'Arthur', 'Fresh', '366')
+book3 = Book(library2, '02.02.2010', 'Agatha', 'Esd', '491')
+book4 = Book(library2, '15.09.2020', 'Jules', 'Bin', '78')
+book5 = Book(library1, '20.09.2002', 'Coss', 'Sin', '100')
 
-student3 = Student('Julia',[100,200,300])
-student4 = Student('Agnieszka',[87,19,20])
-student5 = Student('Wera',[11,2,54])
+employee1 = Employee('Ala', 'Kowalska', '20.01.2021', '01.01.1999',
+                     'Bytom', 'Kwaka', '42-600', '098765432')
+employee2 = Employee('Robert', 'Kowal', '20.11.2021', '12.12.1999',
+                     'Chorzow', 'Lipowa', '42-600', '088323432')
+employee3 = Employee('Macej', 'Polska', '21.01.2021', '09.09.1999',
+                     'T.Gory', 'Lipna', '42-600', '1424162')
 
-order1 = Order(employee1,student5,[book5,book1],'20.09.2022')
-order2 = Order(employee2,student3,[book2,book4,book1],'12.10.2022')
+student3 = Student('Julia', [100, 200, 300])
+student4 = Student('Agnieszka', [87, 19, 20])
+student5 = Student('Wera', [11, 2, 54])
+
+order1 = Order(employee1, student5, [book5, book1], '20.09.2022')
+order2 = Order(employee2, student3, [book2, book4, book1], '12.10.2022')
 
 print(order1)
 print(order2)
